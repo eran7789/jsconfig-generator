65c769d31892097fcfd84592f0cb397e76f9fb37
eran7789 <eran7789@gmail.com>
2019-03-04 18:30:56 +0200

    Added funcitonaliity

--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 const vscode = require('vscode');
 const lodashFP = require('lodash/fp');
+const fs = require('fs');
+const { join } = require('path');
 
 const CONFIG_FLAG = '--config';
 
@@ -8,7 +10,7 @@ function getWebpackScripts(scripts) {
 }
 
 function getConfigsFromScripts(scripts) {
-  return lodashFP.uniq(
+  return lodashFP.flow([
     lodashFP.map(script => {
       if (!script.includes(CONFIG_FLAG)) {
         return require('webpack.config.js');
@@ -21,8 +23,22 @@ function getConfigsFromScripts(scripts) {
       const location = trimmed.slice(0, endIndex === -1 ? trimmed.length : endIndex);
 
       return require(`${vscode.workspace.workspaceFolders[0].uri.path}/${location}`);
-    }, scripts)
-  );
+    }),
+    lodashFP.uniq,
+    lodashFP.compact
+  ])(scripts);
+}
+
+function getPathDirNames(path) {
+  return fs
+    .readdirSync(path)
+    .filter(file => 
+      fs.statSync(join(path, file)).isDirectory()
+    )
+    .map(dir => ({
+      path: join(path, dir),
+      alias: dir
+    }));
 }
 
 /**
@@ -30,14 +46,6 @@ function getConfigsFromScripts(scripts) {
  */
 function activate(context) {
 	let disposable = vscode.commands.registerCommand('jsconfig.generate', function () {
-    // fs.readFile(`${vscode.workspace.workspaceFolders[0].uri.path}/package.json`, 'utf8', (err, data) => {
-    //   if (err) {
-    //     return;
-    //   }
-
-    //   console.log(data);
-    // });
-
     const packageJson = require(`${vscode.workspace.workspaceFolders[0].uri.path}/package.json`);
 
     if (!packageJson) {
@@ -48,9 +56,68 @@ function activate(context) {
 
     const webpackScripts = getWebpackScripts(packageJson.scripts);
     const configs = getConfigsFromScripts(webpackScripts);
+    const aliasPaths = lodashFP.flow([
+      lodashFP.map(config => {
+        return lodashFP.map.convert({ cap: false })((path, alias) => {
+          if (path === 'node_modules') {
+            return null;
+          }
 
+          return { alias, path };
+        }, config.resolve.alias)
+      }),
+      lodashFP.flatten,
+      lodashFP.uniqBy(alias => alias.alias),
+      lodashFP.compact
+    ])(configs);
+    const modulesPaths = lodashFP.flow([
+      lodashFP.map(config => {
+        return lodashFP.map(module => {
+          if (module === 'node_modules') {
+            return null;
+          }
 
-    // console.log(vscode.workspace.workspaceFolders[0].uri.path)
+          return module;
+        }, config.resolve.modules)
+      }),
+      lodashFP.flatten,
+      lodashFP.uniq,
+      lodashFP.compact
+    ])(configs)
+    const modulesPathAndAlias = lodashFP.flatten(modulesPaths.map(getPathDirNames));
+    let paths = {};
+
+    lodashFP.forEach(alias => {
+      paths = lodashFP.set([alias.alias], alias.path, paths);
+    }, aliasPaths);
+    lodashFP.forEach(alias => {
+      paths = lodashFP.set([alias.alias], alias.path, paths);
+    }, modulesPathAndAlias);
+
+    const jsconfig = {
+      compilerOptions: {
+        baseUrl: '.',
+        paths
+      }
+    };
+
+    try {
+      const jsconfigPath = join('.', 'jsconfig.json');
+      // vscode.writeFile(vscode.Uri.file(jsconfigPath), jsconfig, { 
+      //   create: true, 
+      //   overwrite: true 
+      // });
+      // const pathUri = vscode.Uri.file(jsconfigPath);
+      // const fsProvider = vscode.FileSystemProvider.writeFile;
+      // console.log(fsProvider)
+
+      fs.writeFile(jsconfigPath, jsconfig, error => console.log(error));
+    } catch(error) {
+      console.log(error);
+    }
+
+    // console.log([...aliasPaths, ...modulesPathAndAlias]);
+
 		vscode.window.showInformationMessage('hey');
 	});
 
