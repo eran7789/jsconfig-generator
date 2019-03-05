aa0ac1d948d12dd4242958339e94065aa99afac7
eran7789 <eran7789@gmail.com>
2019-03-05 13:34:47 +0200

    Cleaned extension and using utils functions

--- extension.js
+++ extension.js
@@ -1,45 +1,11 @@
 const vscode = require('vscode');
-const lodashFP = require('lodash/fp');
-const fs = require('fs');
-const { join } = require('path');
-
-const CONFIG_FLAG = '--config';
-
-function getWebpackScripts(scripts) {
-  return lodashFP.filter(script => script.includes('webpack'), scripts);
-}
-
-function getConfigsFromScripts(scripts) {
-  return lodashFP.flow([
-    lodashFP.map(script => {
-      if (!script.includes(CONFIG_FLAG)) {
-        return require('webpack.config.js');
-      }
-
-      const startIndex = script.lastIndexOf(CONFIG_FLAG);
-      const fromEndOfConfigToEndString = script.slice(startIndex + CONFIG_FLAG.length); 
-      const trimmed = lodashFP.trimStart(fromEndOfConfigToEndString);
-      const endIndex = trimmed.indexOf(' ');
-      const location = trimmed.slice(0, endIndex === -1 ? trimmed.length : endIndex);
-
-      return require(`${vscode.workspace.workspaceFolders[0].uri.path}/${location}`);
-    }),
-    lodashFP.uniq,
-    lodashFP.compact
-  ])(scripts);
-}
-
-function getPathDirNames(path) {
-  return fs
-    .readdirSync(path)
-    .filter(file => 
-      fs.statSync(join(path, file)).isDirectory()
-    )
-    .map(dir => ({
-      path: join(path, dir),
-      alias: dir
-    }));
-}
+const { 
+  getWebpackScripts, 
+  getConfigsFromScripts 
+} = require('./src/utils/scripts.utils.js');
+const { 
+  getPathsFromConfigs
+} = require('./src/utils/paths.utils');
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -56,44 +22,7 @@ function activate(context) {
 
     const webpackScripts = getWebpackScripts(packageJson.scripts);
     const configs = getConfigsFromScripts(webpackScripts);
-    const aliasPaths = lodashFP.flow([
-      lodashFP.map(config => {
-        return lodashFP.map.convert({ cap: false })((path, alias) => {
-          if (path === 'node_modules') {
-            return null;
-          }
-
-          return { alias, path };
-        }, config.resolve.alias)
-      }),
-      lodashFP.flatten,
-      lodashFP.uniqBy(alias => alias.alias),
-      lodashFP.compact
-    ])(configs);
-    const modulesPaths = lodashFP.flow([
-      lodashFP.map(config => {
-        return lodashFP.map(module => {
-          if (module === 'node_modules') {
-            return null;
-          }
-
-          return module;
-        }, config.resolve.modules)
-      }),
-      lodashFP.flatten,
-      lodashFP.uniq,
-      lodashFP.compact
-    ])(configs)
-    const modulesPathAndAlias = lodashFP.flatten(modulesPaths.map(getPathDirNames));
-    let paths = {};
-
-    lodashFP.forEach(alias => {
-      paths = lodashFP.set([alias.alias], alias.path, paths);
-    }, aliasPaths);
-    lodashFP.forEach(alias => {
-      paths = lodashFP.set([alias.alias], alias.path, paths);
-    }, modulesPathAndAlias);
-
+    const paths = getPathsFromConfigs(configs);
     const jsconfig = {
       compilerOptions: {
         baseUrl: '.',
@@ -101,24 +30,18 @@ function activate(context) {
       }
     };
 
-    try {
-      const jsconfigPath = join('.', 'jsconfig.json');
-      // vscode.writeFile(vscode.Uri.file(jsconfigPath), jsconfig, { 
-      //   create: true, 
-      //   overwrite: true 
-      // });
-      // const pathUri = vscode.Uri.file(jsconfigPath);
-      // const fsProvider = vscode.FileSystemProvider.writeFile;
-      // console.log(fsProvider)
+    vscode.workspace.openTextDocument({ 
+      content: JSON.stringify(jsconfig, null, 2), 
+      language: 'json' 
+    })
+    .then(doc => {
+      if (!doc) {
+        vscode.window.showErrorMessage('Could not create new document');
+      }
 
-      fs.writeFile(jsconfigPath, jsconfig, error => console.log(error));
-    } catch(error) {
-      console.log(error);
-    }
-
-    // console.log([...aliasPaths, ...modulesPathAndAlias]);
-
-		vscode.window.showInformationMessage('hey');
+      vscode.window.showTextDocument(doc);
+      vscode.window.showInformationMessage('Jsconfig proposal is ready in!');
+    });
 	});
 
 	context.subscriptions.push(disposable);
