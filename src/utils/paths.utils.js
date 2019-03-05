790da0db144411c2dbb3731202685ce435b239e5
eran7789 <eran7789@gmail.com>
2019-03-05 13:34:26 +0200

    Added new files for the utils functions

---
+++ src/utils/paths.utils.js
@@ -0,0 +1,65 @@
+const lodashFP = require('lodash/fp');
+const fs = require('fs');
+const { join } = require('path');
+
+export const getPathDirNames = (path) => {
+  return fs
+    .readdirSync(path)
+    .filter(file => 
+      fs.statSync(join(path, file)).isDirectory()
+    )
+    .map(dir => ({
+      path: join(path, dir),
+      alias: dir
+    }));
+}
+
+export const getAliasPathsFromConfigs = (configs) => {
+  return lodashFP.flow([
+    lodashFP.map(config => {
+      return lodashFP.map.convert({ cap: false })((path, alias) => {
+        if (path === 'node_modules') {
+          return null;
+        }
+
+        return { alias, path };
+      }, config.resolve.alias)
+    }),
+    lodashFP.flatten,
+    lodashFP.uniqBy(alias => alias.alias),
+    lodashFP.compact
+  ])(configs);
+}
+
+export const getModulesPathsFromConfigs = (configs) => {
+  return lodashFP.flow([
+    lodashFP.map(config => {
+      return lodashFP.map(module => {
+        if (module === 'node_modules') {
+          return null;
+        }
+
+        return module;
+      }, config.resolve.modules)
+    }),
+    lodashFP.flatten,
+    lodashFP.uniq,
+    lodashFP.compact
+  ])(configs);
+}
+
+export const getPathsFromConfigs = (configs) => {
+  const aliasPaths = getAliasPathsFromConfigs(configs);
+  const modulesPaths = getModulesPathsFromConfigs(configs);
+  const modulesPathAndAlias = lodashFP.flatten(modulesPaths.map(getPathDirNames));
+  let paths = {};
+
+  lodashFP.forEach(alias => {
+    paths = lodashFP.set([alias.alias], alias.path, paths);
+  }, aliasPaths);
+  lodashFP.forEach(alias => {
+    paths = lodashFP.set([alias.alias], alias.path, paths);
+  }, modulesPathAndAlias);
+
+  return paths;
+}
---
+++ src/utils/scripts.utils.js
@@ -0,0 +1,28 @@
+const lodashFP = require('lodash/fp');
+
+const CONFIG_FLAG = '--config';
+
+export const getWebpackScripts = (scripts) => {
+  return lodashFP.filter(script => script.includes('webpack'), scripts);
+}
+
+export const getConfigsFromScripts = (scripts) => {
+  return lodashFP.flow([
+    lodashFP.map(script => {
+      if (!script.includes(CONFIG_FLAG)) {
+        return require('webpack.config.js');
+      }
+
+      const startIndex = script.lastIndexOf(CONFIG_FLAG);
+      const fromEndOfConfigToEndString = script.slice(startIndex + CONFIG_FLAG.length); 
+      const trimmed = lodashFP.trimStart(fromEndOfConfigToEndString);
+      const endIndex = trimmed.indexOf(' ');
+      const location = trimmed.slice(0, endIndex === -1 ? trimmed.length : endIndex);
+
+      return require(`${vscode.workspace.workspaceFolders[0].uri.path}/${location}`);
+    }),
+    lodashFP.uniq,
+    lodashFP.compact
+  ])(scripts);
+}
+
